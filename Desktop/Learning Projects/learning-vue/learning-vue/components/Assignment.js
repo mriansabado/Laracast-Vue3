805d3d8c5106bc314b6f87b8c6e17eb58f903cde
Ian <isabado@onpointglobal.com>
2022-08-19 17:17:42 -0700

    finished section 2 of laracast

---
+++ Desktop/Learning Projects/learning-vue/learning-vue/components/Assignment.js
@@ -0,0 +1,20 @@
+export default {
+    template: `
+    
+    
+    <li>
+            <label>
+                {{ assignment.name }}
+                <input type="checkbox" v-model="assignment.complete">
+            </label>
+     </li>
+    
+    
+    
+    
+    `,
+    props: {
+        assignment: Object
+    }
+
+}
